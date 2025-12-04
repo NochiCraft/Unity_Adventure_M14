3d6ffb9e0c1441f8218d508fd70bc392481f4377
NochiCraft <plastw2@gmail.com>
2025-12-04 09:08:28 +0300

    Наследование от Monobehaviour

---
+++ Assets/InheritanceExample/MonobehExample/MonoBehaviourClass.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoBehaviourClass : MonoBehaviour
+{
+    private int _value;
+
+    public MonoBehaviourClass(int value )
+    {
+        _value = value;
+    }
+}
